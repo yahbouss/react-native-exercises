3bd33c3700eb17fa403520c97edba7b237bb1baa
yahbouss <47297430+yahbouss@users.noreply.github.com>
2022-02-10 14:59:16 +0100

    1st exercise done

--- 1. core-components/App.js
+++ 1. core-components/App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView, Image, Button, Linking } from 'react-native';
+import myPic from "./assets/taswirti.jpg"
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Image
+          source={myPic}
+          style={styles.picture}
+        />
+        <Text style={styles.text}>Yahia Boussaid</Text>
+        <Text style={styles.text}>Life is like the wind, always by my side</Text>
+        <Button
+          title='Connect'
+          style={styles.button}
+          onPress={ ()=>{ Linking.openURL('https://github.com/yahbouss')}}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#3fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  picture:{
+    resizeMode: "contain",
+    height: 400,
+    borderRadius:20
+  },
+  text: {
+    textAlign: "center",
+    fontFamily: 'sans-serif',
+    fontSize: 15,
+    color: "#000",
+    fontWeight: "700",
+    margin: 10,
+  },
+  button:{
+    width: "100%",
+  }
 });
