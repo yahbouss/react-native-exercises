0486ee3408fda3ca90acf37619e996fa09d69ac5
yahbouss <47297430+yahbouss@users.noreply.github.com>
2022-02-10 15:18:53 +0100

    2nd exercise done

--- 2. styling/App.js
+++ 2. styling/App.js
@@ -1,20 +1,34 @@
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Linking } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Bring your colors from{" "}
-        <Text
-          style={{ color: "blue" }}
-          onPress={() => Linking.openURL("https://coolors.co/")}
-        >
-          coolors
-        </Text>{" "}
-        here
-      </Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <Text>
+          Bring your colors from{" "}
+          <Text
+            style={{ color: "blue" }}
+            onPress={() => Linking.openURL("https://coolors.co/")}
+          >
+            coolors
+          </Text>{" "}
+          here
+        </Text>
+        <View style={[styles.box, styles.color1]}>
+          <Text style={{color:"#fff"}}>1st Color</Text>
+        </View>
+        <View style={[styles.box, styles.color2]}>
+          <Text style={{color:"#fff"}}>1st Color</Text>
+        </View>
+        <View style={[styles.box, styles.color3]}>
+          <Text style={{color:"#fff"}}>1st Color</Text>
+        </View>
+        <View style={[styles.box, styles.color4]}>
+          <Text style={{color:"#000"}}>1st Color</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -25,4 +39,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  box:{
+    display: "flex",
+    alignSelf:"center",
+    margin: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 9
+  },
+  color1:{
+
+    backgroundColor:"#540D6E"
+  },
+  color2:{
+    backgroundColor:"#EE4266"
+  },
+  color3:{
+    backgroundColor:"#FFD23F"
+  },
+  color4:{
+    backgroundColor:"#F3FCF0"
+  }
 });
